b1d49ea37549e228343f3470a5e3990f03b50856
Seongeun Yu <s3ich4n@gmail.com>
2023-09-18 07:09:32 +0900

    feat: 코드 내 주석 추가

--- ps/daily/20230916/01-alt3.py
+++ ps/daily/20230916/01-alt3.py
@@ -21,37 +21,43 @@ class Solution:
         return True
 
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        # x, y축을 직관적으로 x, y값으로 세팅함
         arr = [
             [sys.maxsize for _ in range(len(heights[0]))]
             for _ in range(len(heights))
         ]
-        arr[0][0] = 0
+        arr[0][0] = 0       # 시작값을 0으로 시작한다
 
-        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]         # 방향은 이렇게 하면 더 쉽다
         destination = (len(heights) - 1, len(heights[0]) - 1)
         candidates = [(0, 0, 0)]
-        # visited = set()
+        visited = set()     # 방문한 값은 다시 안 가도록 설정한다
 
         while candidates:
             effort, x, y = heapq.heappop(candidates)
-            # visited.add((x, y))
+            visited.add((x, y))
 
+            # 탈출조건: 최우하단 값
             if (x, y) == destination:
                 return effort
 
+            # 사방으로 움직인다
             for dx, dy in directions:
                 nx, ny = x + dx, y + dy
 
-                if self.is_valid(heights, (nx, ny)) and (nx, ny):
+                if self.is_valid(heights, (nx, ny)) and (nx, ny) not in visited:
+                    # 보다 더큰 effort값을 찾는다
                     new_effort = max(effort, abs(heights[x][y] - heights[nx][ny]))
 
+                    # 신규 배열에 가장 작은 값을 저장한다.
+                    # 힙에는 가장 적은 effort 값이 계산되는 루트를 따라가도록 한다.
                     if new_effort < arr[nx][ny]:
                         arr[nx][ny] = new_effort
                         heapq.heappush(candidates, (new_effort, nx, ny))
 
 
 s = Solution()
-# print(s.minimumEffortPath([[1,2,2],[3,8,2],[5,3,5]]))      # 예상 결과: 2
+print(s.minimumEffortPath([[1,2,2],[3,8,2],[5,3,5]]))      # 예상 결과: 2
 # print(s.minimumEffortPath([[1,2,3],[3,8,4],[5,3,5]]))      # 예상 결과: 1
 # print(s.minimumEffortPath([[3]]))      # 예상 결과: 0
 # print(s.minimumEffortPath([[1,10,6,7,9,10,4,9]]))      # 예상 결과: 9
@@ -73,6 +79,6 @@ s = Solution()
 #             [6,4,10,6,10,9,4,6],
 #         ]
 # ))     # 예상 결과: 5
-print(s.minimumEffortPath(
-    [[3],[3],[7],[2],[9],[9],[3],[7],[10]]
-))
+# print(s.minimumEffortPath(
+#     [[3],[3],[7],[2],[9],[9],[3],[7],[10]]
+# ))      # 예상 결과: 7
